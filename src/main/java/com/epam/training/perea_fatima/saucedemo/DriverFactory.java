5a9a6ed4b11ae3c76e3d604b5eed00b2b43e9476
fatiperea <fatimaperea3pppp0@gmail.com>
2026-03-08 17:07:58 -0300

    browsers initialized

---
+++ src/main/java/com/epam/training/perea_fatima/saucedemo/DriverFactory.java
@@ -0,0 +1,22 @@
+package com.epam.training.perea_fatima.saucedemo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DriverFactory {
+
+    public static WebDriver createWebDriver(String browser){
+
+        if (browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        }else if (browser.equalsIgnoreCase("edge")){
+            WebDriverManager.edgedriver().setup();
+            return new EdgeDriver();
+        }
+        throw new IllegalArgumentException(browser + "No soportado");
+
+    }
+}
