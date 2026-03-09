2395b86126f4862b180c5da71a50133b089a5759
fatiperea <fatimaperea3pppp0@gmail.com>
2026-03-09 11:38:38 -0300

    Implement parameterized login cases with CsvSource

--- src/test/java/testLoginPage.java
+++ src/test/java/testLoginPage.java
@@ -1,6 +1,7 @@
 import com.epam.training.perea_fatima.saucedemo.utils.DriverFactory;
 import com.epam.training.perea_fatima.saucedemo.pages.LoginPage;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebDriver;
 
@@ -10,8 +11,21 @@ import static org.hamcrest.Matchers.containsString;
 public class testLoginPage {
 
     @ParameterizedTest
-    @ValueSource( strings = {"chrome", "edge"} )
-    public void testLoginPage(String browser){
+    @CsvSource({
+            // UC-1: both empty
+            "UC-1, chrome, , ",
+            "UC-1, edge, , ",
+
+            // UC-2: empty password
+            "UC-2, chrome, user, ",
+            "UC-2, edge, user, ",
+
+            // UC-3: valid login
+            "UC-3, chrome, standard_user, secret_sauce",
+            "UC-3, edge, standard_user, secret_sauce",
+    })
+    //@ValueSource( strings = {"chrome", "edge"} )
+    public void testLoginPage(String browser, String uc, String user, String password){
 
         WebDriver driver = DriverFactory.createWebDriver(browser);
 
@@ -19,10 +33,22 @@ public class testLoginPage {
 
         LoginPage login = new LoginPage(driver);
 
-        login.login("standard_user", "secret_sauce");
+        login.login(user, password);
 
-        assertThat(login.getErrorMesagge(), containsString("Username and password do not match"));
+        switch (uc){
 
+            case "UC-1":
+                assertThat(login.getErrorMesagge(), containsString("Username is required"));
+                break;
+
+            case "UC-2":
+                assertThat(login.getErrorMesagge(), containsString("Password is required"));
+                break;
+
+            case "UC-3":
+                assertThat(login.getErrorMesagge(), containsString("Swag Labs"));
+                break;
+        }
         driver.quit();
     }
 
