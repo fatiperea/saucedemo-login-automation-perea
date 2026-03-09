095ac94da91902220fd073cd5b0daacd39e706c6
fatiperea <fatimaperea3pppp0@gmail.com>
2026-03-09 00:25:55 -0300

    update JUnit for parameterized tests

--- src/test/java/testDriverFactory.java
+++ src/test/java/testDriverFactory.java
@@ -1,17 +1,40 @@
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebDriver;
 import com.epam.training.perea_fatima.saucedemo.utils.DriverFactory;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 public class testDriverFactory {
 
-    @Test
-    public void testChromeDriverOpensPage() {
+    @ParameterizedTest
+    @ValueSource( strings = {"chrome", "edge", "firefox"} )
+    public void testBrowsers(String browser) {
 
-        WebDriver driver = DriverFactory.createWebDriver("chrome");
+        WebDriver driver = DriverFactory.createWebDriver(browser);
         driver.get("https://www.saucedemo.com/");
+        assertThat(driver.getTitle(), containsString("Swag Labs"));
         driver.quit();
     }
 
+    /*@Test
+    public void testEdgeDriver() {
+
+        WebDriver driver = DriverFactory.createWebDriver("edge");
+        driver.get("https://www.saucedemo.com/");
+        assertThat(driver.getTitle(), containsString("Swag Labs"));
+        driver.quit();
+    }
+
+    @Test
+    public void testFirefoxDriver() {
+
+        WebDriver driver = DriverFactory.createWebDriver("firefox");
+        driver.get("https://www.saucedemo.com/");
+        assertThat(driver.getTitle(), containsString("Swag Labs"));
+        driver.quit();
+    }*/
 
 
 
