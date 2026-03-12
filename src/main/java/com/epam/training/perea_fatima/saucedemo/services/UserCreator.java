49b542667f6c94c89feba7a63e59a535ce2b07fe
fatiperea <fatimaperea3pppp0@gmail.com>
2026-03-12 18:22:33 -0300

    User creator class implemented

--- src/main/java/com/epam/training/perea_fatima/saucedemo/services/UserCreator.java
+++ src/main/java/com/epam/training/perea_fatima/saucedemo/services/UserCreator.java
@@ -1,9 +1,33 @@
 package com.epam.training.perea_fatima.saucedemo.services;
 
+import com.epam.training.perea_fatima.saucedemo.models.User;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 public class UserCreator {
 
     public static final String TESTDATA_USER_NAME = "valid.user";
     public static final String TESTDATA_USER_PASSWORD = "valid.password";
 
+    public static User emptyCredentials(){
+
+        return new User("", "");
+    }
+
+    public static User emptyPassword(String username){
+
+        return new User(username,"");
+
+    }
+
+    public static Stream<User> validUsers(){
+
+        String password=TestDataReader.getValidPassword();
+
+        List<String> usernames= TestDataReader.getAllValidUser();
+
+        return usernames.stream().map(user -> new User(user, password));
+    }
 
 }
