e77b57ede2fbb843eba28e2693e645897905fa53
fatiperea <fatimaperea3pppp0@gmail.com>
2026-03-12 16:55:23 -0300

    dev.properties file added

---
+++ src/main/java/com/epam/training/perea_fatima/saucedemo/services/UserCreator.java
@@ -0,0 +1,9 @@
+package com.epam.training.perea_fatima.saucedemo.services;
+
+public class UserCreator {
+
+    //public static final TESTDATA_USER_NAME =;
+    //public static final TESTDATA_USER_PASSWORD =;
+
+
+}
